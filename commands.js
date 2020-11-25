b434a5b2cd236e6ddeaf36b74f4a7730364efd45
Varga-Somogyi Ákos <vsakos@gmail.com>
2020-11-25 15:40:19 +0100

    Replace exec with spawn

--- commands.js
+++ commands.js
@@ -2,30 +2,44 @@ const { exec, spawn } = require('child_process');
 
 function getStatus () {
   return new Promise((resolve, reject) => {
-    exec('nordvpn status', { timeout: 30 }, (err, stdout, stderr) => {
-      if (err) {
-        reject(err);
-        return;
-      }
+    const stdout = [];
+    const stderr = [];
 
-      const lines = stdout.toString('utf8').split(/[\r\n]/).map((line) => line.trim()).filter((line) => line.length > 3);
-      const items = lines.reduce((acc, line) => {
-        const [ key, value ] = line.split(':');
-        acc[key.trim().toLowerCase()] = value.trim();
-        return acc;
-      }, {});
+    const child = spawn('nordvpn', [ 'status' ]);
 
-      if (items.status === 'Connected') {
-        resolve({
-          connected: true,
-          country: items.country,
-          server: items['current server'],
-          ip: items['your new ip']
-        });
+    child.stdout.on('data', (data) => {
+      stdout.push(data.toString('utf8'));
+    });
+
+    child.stderr.on('data', (data) => {
+      stderr.push(data.toString('utf8'));
+    });
+
+    child.on('close', (code) => {
+      if (code === 0) {
+        const lines = stdout.join('').split(/[\r\n]/).map((line) => line.trim()).filter((line) => line.length > 3);
+        const items = lines.reduce((acc, line) => {
+          const [ key, value ] = line.split(':');
+          if (key && value) {
+            acc[key.trim().toLowerCase()] = value.trim();
+          }
+          return acc;
+        }, {});
+
+        if (items.status === 'Connected') {
+          resolve({
+            connected: true,
+            country: items.country,
+            server: items['current server'],
+            ip: items['your new ip']
+          });
+        } else {
+          resolve({
+            connected: false
+          });
+        }
       } else {
-        resolve({
-          connected: false
-        });
+        reject(stderr.join(''));
       }
     });
   });
@@ -33,6 +47,30 @@ function getStatus () {
 
 function getCountries () {
   return new Promise((resolve, reject) => {
+    const stdout = [];
+    const stderr = [];
+
+    const child = spawn('nordvpn', [ 'countries' ]);
+
+    child.stdout.on('data', (data) => {
+      stdout.push(data.toString('utf8'));
+    });
+
+    child.stderr.on('data', (data) => {
+      stderr.push(data.toString('utf8'));
+    });
+
+    child.on('close', (code) => {
+      if (code === 0) {
+        const lines = stdout.join('').split(/[\r\n]/).map((line) => line.trim()).filter((line) => line.length > 3);
+        const countries = lines.join('').split(',').map((line) => line.trim());
+
+        resolve(countries);
+      } else {
+        reject(stderr.join(''));
+      }
+    });
+
     exec('nordvpn countries', { timeout: 30 }, (err, stdout, stderr) => {
       if (err) {
         reject(err);
